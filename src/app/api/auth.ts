284cfa7dc5aeb5ca90d4f923f84ef7983d099050
Isaac <tux314159@gmail.com>
2023-09-07 03:11:47 +0800

    ofsauwfae

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -23,3 +23,27 @@ export const authorise = async (
   });
   return resp.status == 200;
 };
+
+export const emailChange = async (
+  form: Omit<AccountDetails, 'email' | 'repeat_password'> & { 'newemail': string },
+) => {
+  const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/emailchange`, {
+    cache: 'no-store',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(form),
+  });
+  return resp.status == 200;
+};
+
+export const passwordChange = async (
+  form: Omit<AccountDetails, 'email' | 'repeat_password'> & { 'newpassword': string },
+) => {
+  const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/passwordchange`, {
+    cache: 'no-store',
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(form),
+  });
+  return resp.status == 200;
+};
