92845a329c9b9dd6a77728cf868539490abc0014
Isaac <tux314159@gmail.com>
2023-09-07 12:23:11 +0800

    ffs

--- src/app/api/auth.ts
+++ src/app/api/auth.ts
@@ -3,7 +3,7 @@ import { AccountDetails } from '@/app/types';
 export const registerAccount = async (
   form: Omit<AccountDetails, 'repeat_password'>,
 ) => {
-  const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/signup`, {
+  const resp = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/register`, {
     cache: 'no-store',
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
